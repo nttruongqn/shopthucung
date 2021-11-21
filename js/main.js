1bbe8263ee65362ba77abb0255142f648d0b77e2
nttruongqn <nttruongqn@gmail.com>
2021-11-21 23:55:51 +0700

    truong update lan 4 23h54p 21/11/2021

--- js/main.js
+++ js/main.js
@@ -8,3 +8,28 @@ $(".banner-slider").slick({
 });
 
 
+
+$(".detail-account").click(function() {
+  $(".ma-hello-account").hide();
+  $(".ma-address-account").hide();
+  $(".ma-mng-account").show();
+  $(".ma-cart-infor").hide();
+  $('html, body').animate({ scrollTop: 0 }, 400);
+  
+});
+
+$(".address-account").click(function() {
+  $(".ma-hello-account").hide();
+  $(".ma-address-account").show();
+  $(".ma-mng-account").hide();
+  $(".ma-cart-infor").hide();
+  $('html, body').animate({ scrollTop: 0 }, 400);
+});
+
+$(".ci-account").click(function() {
+  $(".ma-hello-account").hide();
+  $(".ma-address-account").hide();
+  $(".ma-mng-account").hide();
+  $(".ma-cart-infor").show();
+  $('html, body').animate({ scrollTop: 0 }, 400);
+});
